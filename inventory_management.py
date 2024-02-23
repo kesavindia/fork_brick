19fcd7b2ade7a1d5b011e39c966e2f4c069a371b
kesavindia <kesavindia83@gmail.com>
2024-02-23 11:15:26 +0530

    additional features added

--- inventory_management.py
+++ inventory_management.py
@@ -1,4 +1,4 @@
-import csv
+
 class Brick:
     def __init__(self, type, dimensions, weight, color, strength_rating, quantity, cost_per_unit):
         self.type = type
@@ -14,7 +14,6 @@ class Brick:
 
     def calculate_total_cost(self):
         return self.cost_per_unit * self.quantity
-
 
 class RawMaterial:
     def __init__(self, type, supplier, unit, quantity, cost_per_unit):
@@ -52,6 +51,7 @@ class InventoryLocation:
 # Create brick objects
 brick1 = Brick("Standard", (20, 8, 5), 2, "Red", 1500, 150, 25)
 brick2 = Brick("Paving", (20, 20, 5), 3.0, "Gray", 2000, 50, 30)
+brick3 = Brick("Hollow brick",(20,20,20),1.5,"Gray",1000,500,20)
 
 # Create raw material objects
 clay = RawMaterial("Clay", "Supplier A", "Tons", 10, 20)
@@ -61,10 +61,27 @@ sand = RawMaterial("Sand", "Supplier B", "Tons", 5, 15)
 warehouse = InventoryLocation("Warehouse", 1000)
 drying_area = InventoryLocation("Drying Area", 500)
 
+weight_brick1 = brick1.calculate_total_weight()
+print(f"{brick1.type} bricks weight before adding: ",weight_brick1,"kgs")
+weight_brick2 = brick2.calculate_total_weight()
+print(f"{brick2.type} bricks weight before adding: ",weight_brick2,"kgs")
+
 # Add stock to warehouse
 warehouse.add_stock(brick1, 50)
 warehouse.add_stock(brick2, 25)
+drying_area.remove_stock(brick2,10)
+
+
+for item in warehouse.current_stock:
+    print(f"Type: {item.type}, Quantity: {item.quantity}")
+for item in warehouse.current_stock:
+    print(f"Type: {item.type}, Quantity :{item.quantity}")
+items_in_stock = [item for item in warehouse.current_stock]
+
+for item in items_in_stock:
+    print(f"item:{item.type}, Quantity: {item.quantity}")
+for item in items_in_stock:
+    print(f"item:{item.dimensions}, Quantity: {item.cost_per_unit}")
 
 weight_brick1 = brick1.calculate_total_weight()
-
-print(weight_brick1)
+print("bricks weight: ",weight_brick1,"kgs")
