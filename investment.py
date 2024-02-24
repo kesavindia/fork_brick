7a27964622856b68f443d0995a54e28c71500d34
kesavindia <kesavindia83@gmail.com>
2024-02-24 15:06:12 +0530

    investormodule corrected

--- investment.py
+++ investment.py
@@ -1,55 +1,57 @@
-
 class InvestorModule:
     def input_invest(self):
-        self.invest_input = float(input("Enter your Investment Details in Rs : "))
-        self.investment_duration = int(input("Enter investment duration in months: "))
+        print("#####################################################################################")
+        self.initial_investment = 30000000  # Initial investment in manufacturing unit
+        self.productivity = 50000  # Per month
+        self.cost_of_production = float(input("Enter total expenses including raw materials, salaries, and consumables per month: "))
+        self.cost_of_one_brick = float(input("Enter the cost of one brick: "))
+        self.selling_price = self.cost_of_one_brick * self.productivity
+        self.depreciation = (5/100 * 30000000) / 12
+        self.invest_input = float(input("Enter your investment details in Rs: "))
+        print("#####################################################################################")
+    def profit_loss(self):
+        profit = self.selling_price - self.cost_of_production - self.depreciation
+        loss = self.cost_of_production - self.selling_price - self.depreciation
+        return profit, loss
 
-    def profit_loss(self, cost_price, selling_price):
-        profit = selling_price - cost_price
-        loss = cost_price - selling_price
+    def display_investment_details(self, profit, loss):
+        print("#####################################################################################")
+        print("Your initial investment in the manufacturing unit: Rs.", self.initial_investment)
+        print("Your monthly production:", self.productivity, "bricks")
+        print("Your monthly cost of production: Rs.", self.cost_of_production)
+        print("Your selling price per brick: Rs.", self.cost_of_one_brick)
+        print("Your monthly selling price: Rs.", self.selling_price)
+        print("Your monthly depreciation cost: Rs.", self.depreciation)
+        print("Your total investment: Rs.", self.invest_input)
+        print("#####################################################################################")
         if profit > loss:
-            return True, profit
+            print("Your projected profit amount: Rs.", profit)
         else:
-            return False, loss
-
-    def display_investment_details(self, cost_price, selling_price, profit, loss):
-        print("Your total investment is :", self.invest_input)
-        print("Your investment duration is :", self.investment_duration, "months")
-        print("Your projection profit amount is :", profit)
-        print("Your projection loss amount is :", loss)
+            print("Your projected loss amount: Rs.", loss)
 
 
 class Investor(InvestorModule):
     def __init__(self):
         super().__init__()
+
     def input_invest(self):
         super().input_invest()
 
-    def profit_loss(self, cost_price, selling_price):
-        return super().profit_loss(cost_price, selling_price)
+    def profit_loss(self):
+        return super().profit_loss()
 
-    def display_investment_details(self, cost_price, selling_price, profit, loss):
-        super().display_investment_details(cost_price, selling_price, profit, loss)
+    def display_investment_details(self, profit, loss):
+        super().display_investment_details(profit, loss)
 
 
 invest = Investor()
 invest.input_invest()
 
-cost_price = float(input("Enter the cost price : "))
-selling_price = float(input("Enter the Selling price : "))
+profit, loss = invest.profit_loss()
 
-if invest.profit_loss(cost_price, selling_price)[0]:
-    print("The projection amount will be : ")
-    profit = invest.profit_loss(cost_price, selling_price)[1]
-    Percentage = (profit / cost_price) * 100
-    if profit > cost_price:
-        print("Profit",profit)
-        print("Percentage",Percentage)
-    loss = 0
+if profit > loss:
+    print("Congratulations! You're making a profit.")
 else:
-    print("For the investment amount, the projection price will be : ")
-    loss = invest.profit_loss(cost_price, selling_price)[1]
-    profit = 0
+    print("You're experiencing a loss.")
 
-invest.display_investment_details(cost_price, selling_price, profit, loss)
-
+invest.display_investment_details(profit, loss)
